7d310f39e1c3e744e7f29114d6bf9744995dacd6
Filip Procházka <hosiplan@gmail.com>
2011-12-23 20:03:59 +0100

    Added default RobotLoader

--- app/autoload.php
+++ app/autoload.php
@@ -8,7 +8,6 @@
  * @license http://www.kdyby.org/license
  */
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 // load Nette Framework first
@@ -18,14 +17,17 @@ require_once __DIR__ . '/../vendor/nette/Nette/loader.php';
 require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 // libraries
-$loader = new UniversalClassLoader();
+$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Kdyby' => __DIR__ . '/../vendor/framework/libs',
     'Symfony' => __DIR__ . '/../vendor/symfony/src',
-    'Doctrine\\Common' => __DIR__ . '/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL\\Migrations' => __DIR__ . '/../vendor/doctrine-migrations/lib',
-    'Doctrine\\DBAL' => __DIR__ . '/../vendor/doctrine-dbal/lib',
-    'Doctrine\\ORM' => __DIR__ . '/../vendor/doctrine/lib',
+	'Doctrine\\Common\\DataFixtures' => __DIR__ . '/../vendor/doctrine-data-fixtures/lib',
+	'Doctrine\\Common' => __DIR__ . '/../vendor/doctrine-common/lib',
+	'Doctrine\\DBAL\\Migrations' => __DIR__ . '/../vendor/doctrine-migrations/lib',
+	'Doctrine\\DBAL' => __DIR__ . '/../vendor/doctrine-dbal/lib',
+	'Doctrine\\ORM' => __DIR__ . '/../vendor/doctrine/lib',
+	'DoctrineExtensions' => __DIR__ . '/../vendor/doctrine-extensions/lib',
+	'Gedmo' => __DIR__ . '/../vendor/doctrine-gedmo/lib',
 ));
 $loader->registerNamespaceFallbacks(array(
     'Kdyby' => __DIR__ . '/../vendor/cms/libs',
@@ -33,23 +35,15 @@ $loader->registerNamespaceFallbacks(array(
 $loader->register();
 
 // annotations
-AnnotationRegistry::registerLoader(function($class) use ($loader) {
+AnnotationRegistry::registerLoader(function ($class) use ($loader) {
     $loader->loadClass($class);
     return class_exists($class, FALSE);
 });
 AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
-unset($loader); // cleanup
-
-
-// RobotLoader is not suggested
-if (!is_writable(__DIR__ . '/temp')) {
-	throw new Nette\IOException("Temporary directory is not writable.");
-}
-$robot = new Nette\Loaders\RobotLoader;
-$robot->setCacheStorage(new Nette\Caching\Storages\FileStorage(__DIR__ . '/temp'));
-$robot->autoRebuild = TRUE;
-$robot->addDirectory(__DIR__);
+// robot
+$robot = new Kdyby\Loaders\RobotLoader;
+$robot->addDirectory(__DIR__ . '/presenters');
 $robot->register();
 
-unset($robot); // cleanup
+unset($loader, $robot); // cleanup
