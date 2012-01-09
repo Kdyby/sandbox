b36af198ad1d3f97e733c45abd3421da76c2e2f6
Filip Procházka <hosiplan@gmail.com>
2012-01-09 16:59:19 +0100

    Added HeaderControl usage example

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -5,4 +5,20 @@
 abstract class BasePresenter extends Kdyby\Application\UI\Presenter
 {
 
+	public function templatePrepareFilters($template)
+	{
+		parent::templatePrepareFilters($template);
+		Kdyby\Components\Header\HeadMacro::install($this->templateConfigurator->getLatteParser());
+	}
+
+
+
+	/**
+	 * @return Kdyby\Components\Header\HeaderControl
+	 */
+	protected function createComponentHead()
+	{
+		return $this->getContext()->cms_headerControl;
+	}
+
 }
