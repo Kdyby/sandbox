2674d6da0aa36f335c04921d48395ab6111e8ddc
Filip Procházka <hosiplan@gmail.com>
2012-03-30 13:25:13 +0200

    sandbox is a package! http://getcomposer.org/

--- app/autoload.php
+++ app/autoload.php
@@ -12,37 +12,19 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 
 // load Nette Framework first
-require_once __DIR__ . '/../vendor/nette/Nette/loader.php';
+require_once __DIR__ . '/../vendor/nette/nette/Nette/loader.php';
+
 
 // require class loader
-require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__ . '/../vendor/.composer/autoload.php';
+
 
 // libraries
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
 	'App' => __DIR__ . '/libs',
-	'Kdyby' => __DIR__ . '/../vendor/framework/libs',
-	'Symfony' => __DIR__ . '/../vendor/symfony/src',
-	'Doctrine\\Common\\DataFixtures' => __DIR__ . '/../vendor/doctrine-data-fixtures/lib',
-	'Doctrine\\Common' => __DIR__ . '/../vendor/doctrine-common/lib',
-	'Doctrine\\DBAL\\Migrations' => __DIR__ . '/../vendor/doctrine-migrations/lib',
-	'Doctrine\\DBAL' => __DIR__ . '/../vendor/doctrine-dbal/lib',
-	'Doctrine\\ORM' => __DIR__ . '/../vendor/doctrine/lib',
-	'DoctrineExtensions' => __DIR__ . '/../vendor/doctrine-extensions/lib',
-	'Gedmo' => __DIR__ . '/../vendor/doctrine-gedmo/lib',
-	'Assetic' => __DIR__ . '/../vendor/assetic/src',
-));
-$loader->registerNamespaceFallbacks(array(
-    'Kdyby' => __DIR__ . '/../vendor/cms/libs',
 ));
 $loader->register();
-
-// annotations
-AnnotationRegistry::registerLoader(function ($class) use ($loader) {
-    $loader->loadClass($class);
-    return class_exists($class, FALSE);
-});
-AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
 // robot
 $robot = new Kdyby\Loaders\RobotLoader;
